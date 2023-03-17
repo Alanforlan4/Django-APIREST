07992ef9c284614490fa0c7a2c99f3407d8174ed
Alan Gabriel <alangb497@gmail.com>
2023-03-16 23:21:15 -0300

    . Criando APIViews para o método HTTP POST

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -10,10 +11,16 @@ class CursoAPIView(APIView):
     API DE CURSOS
     """
     def get(self, request):
-        print(request.query_params)
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     
 class AvaliacaoAPIView(APIView):
     """
@@ -23,3 +30,8 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
