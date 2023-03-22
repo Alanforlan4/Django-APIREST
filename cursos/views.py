6d0019ef234242429dbb38a87513f6726310b887
Alan Gabriel <alangb497@gmail.com>
2023-03-22 01:11:16 -0300

    Subscrevendo metódos genéricos

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework.generics import get_object_or_404
 
 
 class CursosAPIView(generics.ListCreateAPIView):
@@ -17,8 +18,20 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk')
+                                    )
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
 
